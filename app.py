197d9113cb5fd0293a5cc84eb4bdc6af584cfa35
Matheus Matos Bernardo <matheus.bernardoo1001@gmail.com>
2024-06-23 14:31:28 -0300

    feat: add Delete_user

--- app.py
+++ app.py
@@ -90,7 +90,20 @@ def update_user(id_user):
     return jsonify({"message": "Usuario nao encontrado"}), 404
 
 
+@app.route("/user/<int:id_user>", methods=["DELETE"])
+@login_required
+def delete_user(id_user):
+    user = User.query.get(id_user)
 
+    if id_user == current_user.id:
+        return jsonify({"message": "Nao pode deletar o seu proprio usuario!"}), 403
+
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({"message": f"Usuario {id_user} deletado com sucesso!"})
+
+    return jsonify({"message": "Usuario nao encontrado"}), 404
 
 
 @app.route("/salve", methods=["GET"])
