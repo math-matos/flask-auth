c462e050e7205a8d68eed9f9f4e6a928c41b863f
Matheus Matos Bernardo <matheus.bernardoo1001@gmail.com>
2024-06-23 11:52:53 -0300

    feat: add User in database

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from models.user import User
 from database import db
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
@@ -11,6 +11,27 @@ login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
 
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+                login_user(user)
+                print(current_user.is_authenticated)
+                return jsonify({"message": "Autenticacao realizada com sucesso!"})
+            
+    return jsonify({"message": "Credenciais Invalidas"}), 400
 
 
 @app.route("/salve", methods=["GET"])
