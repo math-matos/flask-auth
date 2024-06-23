646259dc18226a6cbeaa229c02a12dcf24a8e589
Matheus Matos Bernardo <matheus.bernardoo1001@gmail.com>
2024-06-23 12:21:51 -0300

    feat: add logout + create_user

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, jsonify, request
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user
+from flask_login import (
+    LoginManager,
+    login_user,
+    current_user,
+    logout_user,
+    login_required,
+)
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
@@ -11,11 +17,13 @@ login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
 
-login_manager.login_view = 'login'
+login_manager.login_view = "login"
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
+
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -27,11 +35,33 @@ def login():
         user = User.query.filter_by(username=username).first()
 
         if user and user.password == password:
-                login_user(user)
-                print(current_user.is_authenticated)
-                return jsonify({"message": "Autenticacao realizada com sucesso!"})
-            
+            login_user(user)
+            print(current_user.is_authenticated)
+            return jsonify({"message": "Autenticacao realizada com sucesso!"})
+
     return jsonify({"message": "Credenciais Invalidas"}), 400
+
+
+@app.route("/logout", methods=["GET"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Deslogado com sucesso!"})
+
+
+@app.route("/user", methods=["POST"])
+def create_user():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message": "Usuario cadastrado com sucesso!"})
+
+    return jsonify({"message": "Dados invalidos"}), 400
 
 
 @app.route("/salve", methods=["GET"])
