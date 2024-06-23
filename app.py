711387f14b101cc7f941e841d018ce99732d0d66
Matheus Matos Bernardo <matheus.bernardoo1001@gmail.com>
2024-06-23 11:15:30 -0300

    feat: create database

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from models.user import User
+from database import db
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
-db = SQLAlchemy(app)
+login_manager = LoginManager()
+db.init_app(app)
+login_manager.init_app(app)
+
 
 
 @app.route("/salve", methods=["GET"])
